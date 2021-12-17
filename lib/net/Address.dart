0c7965b3706d2958c42b1200fbb26cdd988bd577
903345072 <903345072@qq.com>
2021-12-17 17:50:15 +0800

    asd

--- lib/net/Address.dart
+++ lib/net/Address.dart
@@ -1,5 +1,5 @@
 class Address{
-  static String BASE_URL = "http://8.134.57.41/api/";
-  static String TEST_API ="/v2/movie/in_theaters";
-  static String DOWN_LOAD_URL ="http://39.99.155.47/yibo1";
+    static String BASE_URL = "http://8.134.57.41/api/";
+    static String TEST_API ="/v2/movie/in_theaters";
+    static String DOWN_LOAD_URL ="http://39.99.155.47/yibo1";
 }
